3b2ddca8e20048bbca1708c348e454abf9514c8c
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2024-08-05 21:57:25 +0300

    Adding All The Game Files

---
+++ XOGameProject/XOGAME.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace XOGameProject
+{
+    partial class XOGAME
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.lblTurnResult = new System.Windows.Forms.Label();
+            this.lblWinnerResult = new System.Windows.Forms.Label();
+            this.btnRestartGame = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Font = new System.Drawing.Font("Monotype Corsiva", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.ForeColor = System.Drawing.Color.Yellow;
+            this.lblTurn.Location = new System.Drawing.Point(96, 140);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(110, 56);
+            this.lblTurn.TabIndex = 0;
+            this.lblTurn.Text = "Turn";
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Monotype Corsiva", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.ForeColor = System.Drawing.Color.Yellow;
+            this.lblWinner.Location = new System.Drawing.Point(73, 263);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(156, 56);
+            this.lblWinner.TabIndex = 1;
+            this.lblWinner.Text = "Winner";
+            // 
+            // lblTurnResult
+            // 
+            this.lblTurnResult.AutoSize = true;
+            this.lblTurnResult.Font = new System.Drawing.Font("Monotype Corsiva", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnResult.ForeColor = System.Drawing.Color.Green;
+            this.lblTurnResult.Location = new System.Drawing.Point(75, 196);
+            this.lblTurnResult.Name = "lblTurnResult";
+            this.lblTurnResult.Size = new System.Drawing.Size(179, 45);
+            this.lblTurnResult.TabIndex = 2;
+            this.lblTurnResult.Text = "In Progress";
+            this.lblTurnResult.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblWinnerResult
+            // 
+            this.lblWinnerResult.Font = new System.Drawing.Font("Monotype Corsiva", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinnerResult.ForeColor = System.Drawing.Color.Green;
+            this.lblWinnerResult.Location = new System.Drawing.Point(31, 319);
+            this.lblWinnerResult.Name = "lblWinnerResult";
+            this.lblWinnerResult.Size = new System.Drawing.Size(258, 115);
+            this.lblWinnerResult.TabIndex = 3;
+            this.lblWinnerResult.Text = "In Progress";
+            this.lblWinnerResult.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnRestartGame
+            // 
+            this.btnRestartGame.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnRestartGame.FlatAppearance.BorderSize = 2;
+            this.btnRestartGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestartGame.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestartGame.ForeColor = System.Drawing.Color.White;
+            this.btnRestartGame.Location = new System.Drawing.Point(62, 453);
+            this.btnRestartGame.Name = "btnRestartGame";
+            this.btnRestartGame.Size = new System.Drawing.Size(185, 58);
+            this.btnRestartGame.TabIndex = 4;
+            this.btnRestartGame.Text = "Restart Game";
+            this.btnRestartGame.UseVisualStyleBackColor = true;
+            this.btnRestartGame.Click += new System.EventHandler(this.btnRestartGame_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(371, 22);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(444, 72);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Tick Tac Toe Game";
+            // 
+            // pb9
+            // 
+            this.pb9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb9.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb9.Location = new System.Drawing.Point(681, 383);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(108, 102);
+            this.pb9.TabIndex = 14;
+            this.pb9.TabStop = false;
+            this.pb9.Tag = "9";
+            this.pb9.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb8.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb8.Location = new System.Drawing.Point(545, 383);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(108, 102);
+            this.pb8.TabIndex = 13;
+            this.pb8.TabStop = false;
+            this.pb8.Tag = "8";
+            this.pb8.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb7.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb7.Location = new System.Drawing.Point(409, 383);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(108, 102);
+            this.pb7.TabIndex = 12;
+            this.pb7.TabStop = false;
+            this.pb7.Tag = "7";
+            this.pb7.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb6.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb6.Location = new System.Drawing.Point(681, 258);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(108, 102);
+            this.pb6.TabIndex = 11;
+            this.pb6.TabStop = false;
+            this.pb6.Tag = "6";
+            this.pb6.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb5.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb5.Location = new System.Drawing.Point(545, 258);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(108, 102);
+            this.pb5.TabIndex = 10;
+            this.pb5.TabStop = false;
+            this.pb5.Tag = "5";
+            this.pb5.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb4.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb4.Location = new System.Drawing.Point(409, 258);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(108, 102);
+            this.pb4.TabIndex = 9;
+            this.pb4.TabStop = false;
+            this.pb4.Tag = "4";
+            this.pb4.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb3.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb3.Location = new System.Drawing.Point(681, 133);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(108, 102);
+            this.pb3.TabIndex = 8;
+            this.pb3.TabStop = false;
+            this.pb3.Tag = "3";
+            this.pb3.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb2.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb2.Location = new System.Drawing.Point(545, 133);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(108, 102);
+            this.pb2.TabIndex = 7;
+            this.pb2.TabStop = false;
+            this.pb2.Tag = "2";
+            this.pb2.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb1.Image = global::XOGameProject.Properties.Resources.question_mark_96;
+            this.pb1.Location = new System.Drawing.Point(409, 133);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(108, 102);
+            this.pb1.TabIndex = 6;
+            this.pb1.TabStop = false;
+            this.pb1.Tag = "1";
+            this.pb1.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // XOGAME
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(928, 535);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnRestartGame);
+            this.Controls.Add(this.lblWinnerResult);
+            this.Controls.Add(this.lblTurnResult);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.lblTurn);
+            this.Name = "XOGAME";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.XOGAME_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintingForm);
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Label lblTurnResult;
+        private System.Windows.Forms.Label lblWinnerResult;
+        private System.Windows.Forms.Button btnRestartGame;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb2;
+    }
+}
+
---
+++ XOGameProject/XOGAME.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XOGameProject.Properties;
+
+namespace XOGameProject
+{
+    public partial class XOGAME : Form
+    {
+        public XOGAME()
+        {
+            InitializeComponent();
+        }
+
+
+        //The Global Variables
+        private int playerTurn = 8;
+        private int sizeOfXStatusArray = 0;
+        private int sizeOfOStatusArray = 0;
+        private int[] XStatus = new int[5];
+        private int[] OStatus = new int[5]; //it should be four but I made it 5 for the loop
+
+
+        //The Methods
+        void SpecifyTurn(PictureBox pictureBox)
+        {
+            HandleClickedFields(pictureBox);
+            if(playerTurn % 2 == 0)
+            {
+                playerTurn--;
+                OPlayerTurn(); //Because When X's Played His Turn Next Will Be O's Turn
+                AddXTurn(pictureBox, 'X');
+            }
+            else
+            {
+                playerTurn--;
+                XPlayerTurn(); //Because When O's Played His Turn Next Will Be X's Turn
+                AddOTurn(pictureBox, 'O');
+            }
+        }
+        void HandleClickedFields(PictureBox pictureBox)
+        {
+            pictureBox.Enabled = false;
+        }
+        void XPlayerTurn() 
+        {
+            lblTurnResult.Text = "Player 1 Turn";
+        }
+        void OPlayerTurn()
+        {
+            lblTurnResult.Text = "Player 2 Turn";
+        }
+        void AddXTurn(PictureBox pictureBox, char playerTurn)
+        {
+            pictureBox.Image = Resources.X;
+            SpecifyStatusOfTheGame(pictureBox, playerTurn);
+        }
+        void AddOTurn(PictureBox pictureBox, char playerTurn)
+        {
+            pictureBox.Image = Resources.O;
+            SpecifyStatusOfTheGame(pictureBox, playerTurn);
+        }
+        void SpecifyStatusOfTheGame(PictureBox pictureBox, char playerTurn)
+        {
+            if(this.playerTurn == -1)
+            {
+                GameOver();
+            }
+            WinnerCombinations(pictureBox, playerTurn);
+        }
+        void WinnerCombinations(PictureBox pictureBox, char playerTurn)
+        {
+            int[,] winningCombinations =
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+                {1,5,9},
+                {3,5,7},
+                {1,4,7},
+                {2,5,8},
+                {3,6,9},
+            };
+            if (playerTurn == 'X')
+            {
+                int[] XComb = FindXCombination(pictureBox);
+                CheckWinner(XComb, winningCombinations, playerTurn);
+            }
+            if (playerTurn == 'O')//else
+            {
+                int[] OComb = FindOCombination(pictureBox);
+                CheckWinner(OComb, winningCombinations, playerTurn);
+            }
+            }
+        int[] FindXCombination(PictureBox pictureBox)
+        {
+            int XPlaceTag = Convert.ToInt32(pictureBox.Tag.ToString());
+            XStatus[sizeOfXStatusArray] = XPlaceTag;
+            sizeOfXStatusArray++;
+            return XStatus;
+        }
+        int[] FindOCombination(PictureBox pictureBox)
+        {
+            int OPlaceTag = Convert.ToInt32(pictureBox.Tag.ToString());
+            OStatus[sizeOfOStatusArray] = OPlaceTag;
+            sizeOfOStatusArray++;
+            return OStatus;
+        }
+        void CheckWinner(int[] Comb, int[,] winningCombinations, char player)
+        {
+            int checkWinner = 0;
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    for (int xo = 0; xo < 5; xo++)
+                    {
+                        if (Comb[xo] == winningCombinations[r, c])
+                        {
+                           //{1,3,6,5,9}
+                           //{1,3,5}
+                            checkWinner++;
+                            continue;
+                        }
+                    }
+                }
+                if (checkWinner == 3)
+                {
+                    String statement = player == 'X' ? "Player 1" : "Player 2";
+                    Winner($"{statement} Has Won The Game");
+                    return;
+                }
+            checkWinner = 0;
+            }
+        }
+        void Winner(String winningStatement)
+        {
+            lblWinnerResult.Text = winningStatement;
+            MessageBox.Show(winningStatement, "We have A Winner🏆🥇!!!",MessageBoxButtons.OK);
+            pb1.Enabled = false;
+            pb2.Enabled = false;
+            pb3.Enabled = false;
+            pb4.Enabled = false;
+            pb5.Enabled = false;
+            pb6.Enabled = false;
+            pb7.Enabled = false;
+            pb8.Enabled = false;
+            pb9.Enabled = false;
+        }
+        void GameOver()
+        {
+            lblWinnerResult.Text = "Game Over";
+            MessageBox.Show("We Don't Have A Winner :(", "No Winner!!!", MessageBoxButtons.OK);
+            pb1.Enabled = false;
+            pb2.Enabled = false;
+            pb3.Enabled = false;
+            pb4.Enabled = false;
+            pb5.Enabled = false;
+            pb6.Enabled = false;
+            pb7.Enabled = false;
+            pb8.Enabled = false;
+            pb9.Enabled = false;
+        }
+        void ResetGame()
+        {
+            pb1.Image = Resources.question_mark_96;
+            pb2.Image = Resources.question_mark_96;
+            pb3.Image = Resources.question_mark_96;
+            pb4.Image = Resources.question_mark_96;
+            pb5.Image = Resources.question_mark_96;
+            pb6.Image = Resources.question_mark_96;
+            pb7.Image = Resources.question_mark_96;
+            pb8.Image = Resources.question_mark_96;
+            pb9.Image = Resources.question_mark_96;
+            pb1.Enabled = true;
+            pb2.Enabled = true;
+            pb3.Enabled = true;
+            pb4.Enabled = true;
+            pb5.Enabled = true;
+            pb6.Enabled = true;
+            pb7.Enabled = true;
+            pb8.Enabled = true;
+            pb9.Enabled = true;
+            XStatus = new int[5];
+            OStatus = new int[5];
+            sizeOfXStatusArray = 0;
+            sizeOfOStatusArray = 0;
+            playerTurn = 8;
+            lblTurnResult.Text = "In Progress";
+            lblWinnerResult.Text = "In Progress";
+        }
+
+      
+        //The Event Handlers
+        private void PaintingForm(object sender, PaintEventArgs e)
+        {
+            Color White = Color.FromArgb(255, 255, 255, 255);
+            Pen Pen = new Pen(White);
+            Pen.Width = 10;
+            Pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(Pen, 400, 100, 400, 400);
+            e.Graphics.DrawLine(Pen, 500, 100, 500, 400);
+            e.Graphics.DrawLine(Pen, 300, 200, 600, 200);
+            e.Graphics.DrawLine(Pen, 300, 300, 600, 300);
+        }
+        private void pb_Click(object sender, EventArgs e)
+        {
+            SpecifyTurn((PictureBox)sender);
+        }
+        private void btnRestartGame_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+    }
+}
